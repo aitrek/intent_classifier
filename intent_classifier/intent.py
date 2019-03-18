be73a2ed2db313fe30a37abe0add1b51348cf257
aitrek <aitrek.zh@gmail.com>
2019-03-18 14:26:07 +0800

    update intent.py

--- intent_classifier/intent.py
+++ intent_classifier/intent.py
@@ -1,17 +1,25 @@
 """Class for intent operations - training, predict"""
 
 import os
+import json
 import datetime
 
 import joblib
 import numpy as np
 
-from typing import Tuple
+from typing import Tuple, List
 
 from sklearn.model_selection import GridSearchCV
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import StandardScaler, MultiLabelBinarizer
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
 
 from .base import DataBunch, OneClassClassifier
 from .utils import get_intent_classes
+from .vectorizer import TfidfVectorizerWithEntity
 
 
 DEFAULT_FOLDER = os.path.join(os.getcwd(), "models")
@@ -42,6 +50,7 @@ class Intent:
         self._customer = customer
         self._folder = folder if folder else DEFAULT_FOLDER
         self._classifiers = {}
+        self._mlbs = {}
         if self._check_id(id):
             self._id = id
             self._load()
@@ -65,25 +74,110 @@ class Intent:
         intent_classes = get_intent_classes(data_bunch.intents)
         for clf_name, cls in intent_classes.items():
             choices = np.char.startswith(cls, clf_name)
-            words = data_bunch.words[choices]
-            context = data_bunch.contexts[choices]
-            targets = data_bunch.intents[choices]
+            classes = np.unique(data_bunch.intents[choices])
+            # todo report
+            if len(classes) == 1:
+                self._classifiers[clf_name] = OneClassClassifier(clf_name)
+            else:
+                mlb = MultiLabelBinarizer(classes=classes)
+                self._classifiers[clf_name] = self._fit(
+                    X=(data_bunch.words[choices],                   # np.array
+                       [json.loads(c) if c else {}
+                        for c in data_bunch.contexts[choices]]),    # List[dict]
+                    y=mlb.fit_transform(data_bunch.intents[choices])
+                )
+                self._mlbs[clf_name] = mlb
 
+    def _fit(self, X: Tuple[np.array, List[dict]], y: np.array):
+        """Fit classifier
 
+        Parameters
+        ----------
+        # X: pd.DataFrame with columns "words" and "contexts".
+        X: tuple of "words" and "contexts".
+        y: intent labels
 
-    def _fit(self, ):
+        Returns
+        -------
+        Instance of sklearn classifier or OneClassClassifier.
 
-    def predict(self, text: str="", context: dict=None):
+        """
+        pipeline = Pipeline([
+            # transform words and contexts to vectors
+            ("vectorizer", ColumnTransformer([
+                # words to vectors
+                ("words2vect", TfidfVectorizerWithEntity(ner=self._ner), 0),
+                # contexts to vectors
+                ("contexts2vect", DictVectorizer(), 1)])
+            ),
+
+            # feature values standardization
+            ("scaler", StandardScaler()),
+
+            # dimensionality reduction
+            ("pca", PCA(n_components="mle")),
+
+            # classifier
+            ("clf", RandomForestClassifier())
+        ])
+        params = {
+            "clf__n_estimators": range(5, 100, 5),
+            "clf__max_features": [None, "sqrt", "log2"],
+            "clf__class_weight": ["balanced", "balanced_subsample"]
+        }
+        search = GridSearchCV(estimator=pipeline, param_grid=params, cv=5)
+        search.fit(X, y)
+
+        return search
+
+    def predict(self, word: str="", context: dict=None) -> List[str]:
         """
 
         Parameters
         ----------
-        text
+        word
         context
 
         Returns
         -------
+        List of predicted labels.
 
+        """
+        return self._predict("root", word, context)
+
+    def _predict(self, intent: str, word: str="", context: dict=None) -> List[str]:
+        """
+        Predict labels using classifiers and multilabelbinarizers.
+
+        Parameters
+        ----------
+        intent: intent name
+        word: user input
+        context: context information
+
+        Returns
+        -------
+        Tuple of predicted labels.
+
+        """
+        intent_labels = []
+        if isinstance(self._classifiers[intent], OneClassClassifier):
+            intent_labels.append(
+                self._classifiers[intent].predict(word, context))
+        else:
+            for name in self._mlbs[intent].inverse_transform(
+                    self._classifiers[intent].predict(word, context)):
+                intent_label = intent + "/" + name
+                if intent_label in self._classifiers:
+                    intent_labels += self._predict(intent_label, word, context)
+                else:
+                    intent_labels.append(intent_label)
+
+        return intent_labels
+
+    def report(self):
+        """
+        Create classifiers' reports and save them in self._folder/self._id.
         """
         pass
 
@@ -105,83 +199,23 @@ class Intent:
         if not id:
             return False
         else:
-            clf_folder = os.path.join(self._folder, self._customer, id)
-            clf_root = os.path.join(clf_folder, "root.clf")
-            return os.path.isdir(clf_folder) and os.path.isfile(clf_root)
+            return os.path.isfile(
+                os.path.join(self._folder, self._customer, id) + ".models")
 
     def _load(self):
         """Load classifiers according to initialized parameters"""
-        self._classifiers = {}
-
-        clf_folder = os.path.join(self._folder, self._customer, self._id)
-        start = len(clf_folder)
-
-        for root, _, files in os.walk(clf_folder):
-            clf_root = root[start:]
-            for file in files:
-                clf_path = os.path.join(root, file)
-                clf_key = os.path.join(clf_root, ".".join(file.split(".")[:-1]))
-                self._classifiers[clf_key] = joblib.load(clf_path)
+        models = joblib.load(
+            os.path.join(self._folder, self._customer, self._id) + ".models"
+        )
+        self._classifiers = models["clfs"]
+        self._mlbs = models["mlbs"]
 
     def _dump(self):
-        """Save classifiers
-
-        To make it easy to examine the classifiers, the classifiers will
-        be save in folder and sub-folder according the intent levels.
-
-        Suffix of the classifiers:
-        1.sklearn classifier - clf
-        2.OneClassClassifier instance - occlf
-
-        Examples:
-             Intents:
-                news/sports_news/football
-                news/sports_news/basketball
-                news/sports_news/others
-                news/tech_news
-                news/social_news
-                travel/culture
-                travel/food
-                shop/clothes
-                chat/greeting
-                chat/others
-                confirm
-                unconfirm
-                others
-
-             Intents folder structure:
-                |===root.clf                # classes: news, travel, shop, chat, confirm, unconfirm, others
-                |---news
-                    |===news.clf            # classes: sports_news, tech_news, social_news
-                    |---sports_news
-                        |===sports_news.clf # classes: football, basketball, others
-                |---travel
-                    |---travel.clf          # classes: culture, food
-                |---shop
-                    |---clothes.occlf       # one-class: clothes
-                |---chat
-                    |---chat.clf            # classes: greeting, others
-
         """
-        for key, clf in self._classifiers.items():
-            paths = key.split("/")
-            if len(paths) == 1:
-                clf_dir = os.path.join(self._folder, self._customer, self._id)
-                clf_name = paths[0]
-            else:
-                clf_dir = os.path.join(self._folder, self._customer, self._id,
-                                       *paths[:-1])
-                clf_name = paths[-1]
-
-            if isinstance(clf, OneClassClassifier):
-                clf_name += ".occlf"
-            else:
-                clf_name += ".clf"
-
-            # create dir if not exists
-            if not os.path.isdir(clf_dir):
-                os.mkdir(clf_dir)
-
-            joblib.dump(self._classifiers, os.path.join(clf_dir, clf_name))
-
-
+        Save classifiers in self._folder/self._cumstomer/self._id + ".models"
+        """
+        clf_dir = os.path.join(self._folder, self._customer)
+        if not os.path.isdir(clf_dir):
+            os.mkdir(clf_dir)
+        joblib.dump({"clfs": self._classifiers, "mlbs": self._mlbs},
+                    clf_dir + self._id + ".models")
