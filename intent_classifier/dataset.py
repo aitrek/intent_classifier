0e03d63a10bca6467a1826d43d4f1ad9bf7e04a8
aitrek <aitrek.zh@gmail.com>
2019-03-21 14:05:25 +0800

    add function to load dataset from csv file

--- intent_classifier/dataset.py
+++ intent_classifier/dataset.py
@@ -58,17 +58,66 @@ def load_from_mysql(configs: dict) -> DataBunch:
             format(db=configs["db"], table=configs["table"])
 
     cursor.execute(sql)
-    for word, context, intent in cursor.fetchall():
-        if not intent:
+    for word, context, intent_labels in cursor.fetchall():
+        if not intent_labels:
             continue
         if not word and not context:
             continue
         words.append(word.lower()) if word else words.append("")
         contexts.append(context.lower()) if context else contexts.append("{}")
-        intents.append(intent.lower())
+        intents.append(intent_labels.lower())
 
     cursor.close()
     db.close()
+
+    return DataBunch(words=np.array(words, dtype=np.str),
+                     contexts=np.array(contexts, dtype=np.str),
+                     intents=np.array(intents, dtype=np.str))
+
+
+def load_from_csv(csv_path: str) -> DataBunch:
+    """
+    Load intent dataset from csv file, which has fields:
+    words - user's words.
+            It could an empty string "" if no word input, like predict user's
+            intent merely by context information.
+    context - json format string.
+            If no context information, it could as well be an empty string ""
+            or "{}".
+    intent_labels - labels of intent.
+            String separated with ",", such as
+            "news/sports/football,person/story", which means labels
+            "news/sports/football" and "person/story".
+
+    Parameters
+    ----------
+    csv_path: the path of the dataset file.
+
+    Returns
+    -------
+    Sklearn Bunch instance, including attributes:
+    words - strings, user's words
+    context - string in json format to offer extended features of context
+    intent_labels - string, intent name in form of multi-levels
+        separated with "," for multi-labels, such as
+        "news/sports/football,person/story", which means labels
+        "news/sports/football" and "person/story".
+
+    """
+    import csv
+
+    with open(csv_path) as f:
+        csv_file = csv.reader(f)
+        next(csv_file)      # skip the header
+        words = []
+        contexts = []
+        intents = []
+        for word, context, intent_labels in csv_file:
+            if not word and (not context or context.strip()=="{}"):
+                continue
+            words.append(word)
+            contexts.append(context) if context else context.append("{}")
+            intents.append(intent_labels)
 
     return DataBunch(words=np.array(words, dtype=np.str),
                      contexts=np.array(contexts, dtype=np.str),
