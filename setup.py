5cb860f2415bf2479add2bcc618b62ed17c007c4
aitrek <aitrek.zh@gmail.com>
2019-03-26 18:04:32 +0800

    update version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="intent_classifier",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["numpy", "scipy", "PyMySQL", "scikit-learn==0.20.3"]
